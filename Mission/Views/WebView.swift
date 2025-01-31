79e00956a6c34145a4a9c2fb28e432b30b7269f7
Steve2Go <steve@grigg.me>
2025-01-31 17:18:02 +1000

    File browser, on ios, refinements

---
+++ Mission/Views/WebView.swift
@@ -0,0 +1,18 @@
+//
+//  WebView.swift
+//  Throttle for Transmission
+//
+//  Created by Stephen Grigg on 28/1/2025.
+//
+
+import SwiftUI
+
+struct WebView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WebView()
+}
